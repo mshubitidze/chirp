38135e37ab51ce2e9fff97313e27a4ca9df08ec2
mshubitidze <59510085+mshubitidze@users.noreply.github.com>
2023-03-29 22:20:08 +0400

    font sizing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -53,7 +53,7 @@ const PostView = (props: PostWithUser) => {
           <span>{`@${author.username}`}</span> ·{" "}
           <span className="font-thin">{dayjs(post.createdAt).fromNow()}</span>
         </div>
-        <span>{post.content}</span>
+        <span className="text-2xl">{post.content}</span>
       </div>
     </div>
   );
