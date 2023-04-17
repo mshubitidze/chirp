8c606ff5f3b40787170f60587cc4fbfc146ef7dc
mshubitidze <59510085+mshubitidze@users.noreply.github.com>
2023-04-17 19:02:06 +0400

    testing

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,11 +1,20 @@
+import Link from "next/link";
 import type { PropsWithChildren } from "react";
 
 export const PageLayout = (props: PropsWithChildren) => {
   return (
-    <main className="flex h-screen justify-center">
-      <div className="h-full w-full border-x border-slate-400 md:max-w-2xl overflow-y-scroll">
-        {props.children}
-      </div>
-    </main>
+    <>
+      <main className="flex h-screen flex-col items-center justify-center">
+        <div className="h-full w-full overflow-y-scroll border-x border-slate-400 md:max-w-2xl">
+          {props.children}
+        </div>
+        <Link
+          href="https://github.com/mshubitidze"
+          className="flex w-full items-center justify-center border-t border-slate-400 p-2 text-slate-200"
+        >
+          @mshubitidze
+        </Link>
+      </main>
+    </>
   );
 };
