477935e844433dd88fd19126790091fed1f98900
mshubitidze <59510085+mshubitidze@users.noreply.github.com>
2023-05-31 15:31:10 +0400

    fix footer

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -8,12 +8,14 @@ export const PageLayout = (props: PropsWithChildren) => {
         <div className="h-full w-full overflow-y-scroll border-x border-slate-400 md:max-w-2xl">
           {props.children}
         </div>
-        <Link
-          href="https://github.com/mshubitidze"
-          className="flex w-full items-center justify-center border-t border-slate-400 p-2 text-slate-200"
-        >
-          @mshubitidze
-        </Link>
+        <footer className="flex w-full items-center justify-center border-t p-2">
+          <Link
+            href="https://github.com/mshubitidze"
+            className="hover:underline hover:underline-offset-2"
+          >
+            @mshubitidze
+          </Link>
+        </footer>
       </main>
     </>
   );
