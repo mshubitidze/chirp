687d8a82e1df0bc6fe6d315efb79a4f0f5b8b80f
mshubitidze <59510085+mshubitidze@users.noreply.github.com>
2023-06-02 17:34:01 +0400

    move to railway db, fix layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,20 +3,20 @@ import type { PropsWithChildren } from "react";
 
 export const PageLayout = (props: PropsWithChildren) => {
   return (
-    <>
-      <main className="flex h-screen flex-col items-center justify-center">
+    <div className="flex h-screen flex-col">
+      <main className="flex flex-1 flex-col items-center justify-center">
         <div className="h-full w-full overflow-y-scroll border-x border-slate-400 md:max-w-2xl">
           {props.children}
         </div>
-        <footer className="flex w-full items-center justify-center border-t p-2">
-          <Link
-            href="https://github.com/mshubitidze"
-            className="hover:underline hover:underline-offset-2"
-          >
-            @mshubitidze
-          </Link>
-        </footer>
       </main>
-    </>
+      <footer className="flex w-full items-center justify-center border-t p-2">
+        <Link
+          href="https://github.com/mshubitidze"
+          className="hover:underline hover:underline-offset-2"
+        >
+          @mshubitidze
+        </Link>
+      </footer>
+    </div>
   );
 };
